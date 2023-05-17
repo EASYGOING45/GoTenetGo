1dc2c9023dceedeb882117aaa8aebbee27944632
EASYGOING45 <1505674928@qq.com>
2023-05-17 15:58:53 +0800

    初识go
    初识go

---
+++ Go-1/变量.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a = "initial"
+	var b, c int = 1, 2
+	var d = true
+	var e float64
+	f := float32(e)
+
+	g := a + "tenet"
+	fmt.Println(a, b, c, d, e, f)
+	fmt.Println(g)
+
+	const s string = "const"
+	const h = 500000000
+	const i = 3e20 / h
+	fmt.Println(s, h, i, math.Sin(h), math.Sin(i))
+
+}
